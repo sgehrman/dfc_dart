9098192fcc5f1619719fadaa02bc4a43876dbe44
Steve Gehrman <sgehrman@gmail.com>
2025-06-09 17:30:13 -0700

    sdf

--- lib/src/string_ext.dart
+++ lib/src/string_ext.dart
@@ -1,3 +1,5 @@
+import 'package:dfc_dart/src/utils.dart';
+
 extension StrExts on String {
   // case insensitive compare
   bool equals(String? other) {
@@ -6,5 +8,18 @@ extension StrExts on String {
     }
 
     return toLowerCase() == other.toLowerCase();
+  }
+
+  String removeTrailing(String trailing) {
+    // don't remove last '/' if path is '/'
+    if (this != trailing) {
+      if (Utls.isNotEmpty(trailing)) {
+        if (endsWith(trailing)) {
+          return substring(0, length - trailing.length);
+        }
+      }
+    }
+
+    return this;
   }
 }
